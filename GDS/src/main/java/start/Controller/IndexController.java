ed745023554d3ae424d45a95acc59e4217d1a568
Renato Nascimento <renatonascimento4567@gmail.com>
2024-08-29 08:16:19 -0300

    feat: js e import sql

--- GDS/src/main/java/start/Controller/IndexController.java
+++ GDS/src/main/java/start/Controller/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index () {
         return "index";
     }
+
+    @GetMapping("/gestao-sonhos")
+    public String gestaoSonhos (){
+        return "gestaodesonhos";
+    }
 }
